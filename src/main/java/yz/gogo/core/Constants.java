5862465a5dd2d9dd6548900bdb51ddd22ccc5c7a
zenuo <zenuo@users.noreply.github.com>
2018-06-15 20:58:04 +0800

    修复typo

--- src/main/java/yz/gogo/core/Constants.java
+++ src/main/java/yz/gogo/core/Constants.java
@@ -14,18 +14,21 @@ public final class Constants {
      * 监听端口
      */
     public static final int PORT = Integer.parseInt(System.getProperty("port", "8080"));
+
     /**
      * 替换规则的映射
      */
     public static final Map<String, String> SUBSTITUTE_RULE_MAP = new HashMap<>();
+
     /**
      * 替换规则的正则
      */
     public static final Pattern SUBSTITUTE_RULE_PATTERN = Pattern.compile("^\"(.+?)\" \"(.*?)\"$");
+
     /**
      * URL前缀
      */
-    private static final String URL_REFIX = System.getProperty("google.search.host", "https://www.google.com");
+    private static final String URL_PREFIX = System.getProperty("google.search.host", "https://www.google.com");
 
     /**
      * 请求超时，毫秒
@@ -50,11 +53,11 @@ public final class Constants {
     /**
      * 谷歌搜索URL模板
      */
-    public static final String GOOGLE_SEARCH_URL_TEMPLATE = URL_REFIX +
+    public static final String GOOGLE_SEARCH_URL_TEMPLATE = URL_PREFIX +
             "/search?safe=strict&q=%s&start=%d";
     /**
      * 谷歌搜索补全URL模板
      */
-    public static final String GOOGLE_SEARCH_COMPLETE_URL_TEMPLATE = URL_REFIX +
+    public static final String GOOGLE_SEARCH_COMPLETE_URL_TEMPLATE = URL_PREFIX +
             "/complete/search?client=psy-ab&q=%s";
 }
