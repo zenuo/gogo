7a481f34e4be92817edb03b57fa06a3e08d51942
zenuo <zenuo@users.noreply.github.com>
2018-06-22 22:47:56 +0800

    同步用户代理类的get方法

--- src/main/java/yz/gogo/util/UserAgentUtils.java
+++ src/main/java/yz/gogo/util/UserAgentUtils.java
@@ -2,8 +2,6 @@ package yz.gogo.util;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * 用户代理工具类
  */
@@ -22,18 +20,18 @@ final class UserAgentUtils {
     /**
      * 最后一次使用的索引
      */
-    private static AtomicInteger INDEX = new AtomicInteger(0);
+    private static int INDEX = 0;
 
     /**
      * 获取用户代理字符串
      *
      * @return 用户代理字符串
      */
-    static String get() {
-        if (INDEX.get() > USER_AGENTS.length - 1) {
+    synchronized static String get() {
+        if (INDEX > USER_AGENTS.length - 1) {
             //重置
-            INDEX.set(0);
+            INDEX = 0;
         }
-        return USER_AGENTS[INDEX.getAndIncrement()];
+        return USER_AGENTS[INDEX++];
     }
 }
---
+++ src/test/java/yz/gogo/util/UserAgentUtilsTest.java
@@ -0,0 +1,14 @@
+package yz.gogo.util;
+
+import org.junit.Test;
+
+public class UserAgentUtilsTest {
+    @Test
+    public void get() {
+        System.out.println(UserAgentUtils.get());
+        System.out.println(UserAgentUtils.get());
+        System.out.println(UserAgentUtils.get());
+        System.out.println(UserAgentUtils.get());
+        System.out.println(UserAgentUtils.get());
+    }
+}
