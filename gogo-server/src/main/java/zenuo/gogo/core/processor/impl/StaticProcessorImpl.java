280288323d37cba95915849b3226abd5ef0c9528
yuanzhen <zenuo@protonmail.com>
2020-07-26 15:08:54 +0800

    set content type: file

--- gogo-server/src/main/java/zenuo/gogo/core/processor/impl/StaticProcessorImpl.java
+++ gogo-server/src/main/java/zenuo/gogo/core/processor/impl/StaticProcessorImpl.java
@@ -1,7 +1,12 @@
 package zenuo.gogo.core.processor.impl;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import lombok.extern.slf4j.Slf4j;
@@ -32,5 +37,28 @@ public final class StaticProcessorImpl implements IStaticProcessor {
         } catch (Exception e) {
             log.error("process static '{}' error", path, e);
         }
+    }
+
+
+    @Override
+    public void response(
+            final ChannelHandlerContext ctx,
+            final FullHttpRequest request,
+            final ResponseType responseType,
+            final byte[] body,
+            final HttpResponseStatus status
+    ) {
+        //响应对象
+        final DefaultFullHttpResponse response = new DefaultFullHttpResponse(
+                request.protocolVersion(),
+                status,
+                body == null ? Unpooled.buffer() : Unpooled.copiedBuffer(body));
+        //设置头信息
+        response.headers().add(HttpHeaderNames.SERVER, "gogo");
+        response.headers().add(HttpHeaderNames.CACHE_CONTROL, "private, max-age=120");
+        response.headers().add(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.FILE);
+        //响应后关闭通道
+        ctx.writeAndFlush(response)
+                .addListener(ChannelFutureListener.CLOSE);
     }
 }
