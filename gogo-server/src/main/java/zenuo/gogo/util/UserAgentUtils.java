32fa88a675ccc5201a737203057638ce5a42f1e9
zenuo <zenuo@protonmail.com>
2022-11-11 23:53:36 +0800

    use Lynx user agent bypass google search client side rendering

--- gogo-server/src/main/java/zenuo/gogo/util/UserAgentUtils.java
+++ gogo-server/src/main/java/zenuo/gogo/util/UserAgentUtils.java
@@ -2,8 +2,7 @@ package zenuo.gogo.util;
 
 /**
  * 用户代理工具类
- * 
- * https://www.zytrax.com/tech/web/mobile_ids.html
+ *
  */
 public final class UserAgentUtils {
 
@@ -11,10 +10,10 @@ public final class UserAgentUtils {
      * 用户代理字符串数组
      */
     private static final String[] USER_AGENTS = {
-            "Mozilla/5.0 (Mobile; Nokia 8110 4G; rv:46.0) Gecko/46.0 Firefox/46.0 KAIOS/2.5",
-            "Mozilla/5.0 (Mobile; Nokia 8110 4G; rv:47.0) Gecko/47.0 Firefox/47.0 KAIOS/2.5",
-            "Mozilla/5.0 (Mobile; Nokia 8110 4G; rv:48.0) Gecko/48.0 Firefox/48.0 KAIOS/2.5",
-            "Mozilla/5.0 (Mobile; Nokia 8110 4G; rv:49.0) Gecko/49.0 Firefox/49.0 KAIOS/2.5",
+            "Lynx/2.8.5rel.2 libwww-FM",
+            "Lynx/2.7.5rel.1 libwww-FM",
+            "Lynx/1.9.5rel.9 libwww-FM",
+            "Lynx/1.8.1rel.3 libwww-FM",
     };
     /**
      * 最后一次使用的索引
