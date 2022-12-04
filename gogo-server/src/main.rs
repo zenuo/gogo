0789e232eda2ac7ea61e8211a3ea664bbbd20bb7
yuanzhen <zenuo@protonmail.com>
2022-12-04 17:40:06 +0800

    prepare for suggest

--- gogo-server/src/main.rs
+++ gogo-server/src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use html5ever::tendril::TendrilSink;
 use once_cell::sync::{Lazy, OnceCell};
-use reqwest::Client;
+use reqwest::{Client, RequestBuilder};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::sync::atomic::AtomicU8;
@@ -28,9 +28,9 @@ struct ResultEntry {
 }
 
 #[derive(Deserialize, Serialize)]
-struct SearchResponse {
+struct GogoResponse<T> {
     error: Option<String>,
-    result: Option<VecDeque<ResultEntry>>, // todo result
+    result: Option<T>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -54,7 +54,9 @@ static CONFIG: OnceCell<Config> = OnceCell::new();
 static HTTP_CLIENT: Lazy<Client> = Lazy::new(|| {
     let config = CONFIG.get().expect("config is not initialized");
     reqwest::ClientBuilder::new()
-        .connect_timeout(Duration::from_millis(config.http_client_connect_timeout_millis))
+        .connect_timeout(Duration::from_millis(
+            config.http_client_connect_timeout_millis,
+        ))
         .danger_accept_invalid_certs(true)
         .connection_verbose(true)
         .pool_max_idle_per_host(config.http_client_pool_max_idle_per_host)
@@ -71,12 +73,13 @@ async fn main() {
     init_config(config_file);
     let static_path = warp::fs::dir(&CONFIG.get().expect("").static_path);
     let listen_address: SocketAddr =
-        SocketAddr::from_str(&CONFIG.get().expect("msg").listen_address).expect("Invalid listen address");
+        SocketAddr::from_str(&CONFIG.get().expect("msg").listen_address)
+            .expect("Invalid listen address");
     let api = warp::path("api");
     let search = api
         .and(warp::path("search"))
         .and(warp::query::<SearchRequest>())
-        .and_then(render_response);
+        .and_then(render_response_search);
     warp::serve(search.or(static_path))
         .run(listen_address)
         .await;
@@ -90,12 +93,7 @@ fn init_config(config_file: File) {
     CONFIG.set(config);
 }
 
-async fn fetch(request: SearchRequest) -> Result<String, reqwest::Error> {
-    let start = if request.p > 1 {
-        (request.p - 1) * 10
-    } else {
-        0
-    };
+fn user_agent() -> &'static str {
     let index_value = USER_AGENT_INDEX.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
     if index_value > 199 {
         USER_AGENT_INDEX.store(0, std::sync::atomic::Ordering::SeqCst);
@@ -108,10 +106,12 @@ async fn fetch(request: SearchRequest) -> Result<String, reqwest::Error> {
         Some(m) => m,
         None => "Lynx/2.8.5rel.2 libwww-FM",
     };
-    let res = HTTP_CLIENT
-        .get(format!("{}/search", config.google_base_url))
-        .query(&[("q", request.q), ("start", start.to_string())])
-        .header("user-agent", user_agent)
+    return user_agent;
+}
+
+async fn fetch(request: RequestBuilder) -> Result<String, reqwest::Error> {
+    let res = request
+        .header("user-agent", user_agent())
         .send()
         .await?
         .text()
@@ -119,12 +119,23 @@ async fn fetch(request: SearchRequest) -> Result<String, reqwest::Error> {
     Ok(res)
 }
 
-async fn render_response(request: SearchRequest) -> Result<impl warp::Reply, warp::Rejection> {
-    let resp = fetch(request).await;
+async fn render_response_search(
+    request: SearchRequest,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let config = CONFIG.get().expect("config is not initialized");
+    let start = if request.p > 1 {
+        (request.p - 1) * 10
+    } else {
+        0
+    };
+    let http_request = HTTP_CLIENT
+        .get(format!("{}/search", config.google_base_url))
+        .query(&[("q", request.q), ("start", start.to_string())]);
+    let resp = fetch(http_request).await;
     match resp {
         Ok(body) => {
             let result_enteries = kuchiki(body);
-            let response = SearchResponse {
+            let response = GogoResponse {
                 error: None,
                 result: Some(result_enteries),
             };
@@ -193,10 +204,13 @@ fn kuchiki(body: String) -> VecDeque<ResultEntry> {
 
 #[cfg(test)]
 mod tests {
+    use serde_json::Value;
+
     use crate::fetch;
-    use crate::kuchiki;
     use crate::init_config;
-    use crate::SearchRequest;
+    use crate::kuchiki;
+    use crate::CONFIG;
+    use crate::HTTP_CLIENT;
     use std::{fs::File, io::Read, path::Path};
 
     #[test]
@@ -212,20 +226,23 @@ mod tests {
     #[tokio::test]
     async fn fetch_works() {
         init_config(File::open("config.json").expect("Unable to open file: config.json"));
-        let search_request = SearchRequest {
-            q: "udp".to_string(),
-            p: 1,
-        };
-        let result = fetch(search_request).await;
+        let config = CONFIG.get().expect("config is not initialized");
+        let http_request = HTTP_CLIENT
+            .get(format!("{}/search", config.google_base_url))
+            .query(&[("q", "udp"), ("start", "0")]);
+        let result = fetch(http_request).await;
         assert!(result.is_ok());
     }
 
     #[test]
-    fn complete_works() {
-        for page in std::fs::read_dir("test/complete").unwrap() {
+    fn suggest_works() {
+        for page in std::fs::read_dir("test/suggest").unwrap() {
             let path = page.unwrap().path();
             let body = read_file(path.as_path());
-            // todo
+            let json_value: Value = serde_json::from_str(&body).expect("invalid complete json");
+            let array: &Vec<Value> = json_value[1].as_array().expect("without second item");
+            let suggestions: Vec<&str> = array.iter().map(|i| i.as_array().expect("without first item")[0].as_str().expect("msg")).collect();
+            assert!(suggestions.len() != 0);
         }
     }
 
